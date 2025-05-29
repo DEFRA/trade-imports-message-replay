2f62d531aed610434619e3605bf7fcc493f5f4f5
Thomas Anderson <t11omas1983@live.co.uk>
2025-05-29 14:50:04 +0100

    fixed processor

--- src/MessageReplay/Services/IImportProcessorApi.cs
+++ src/MessageReplay/Services/IImportProcessorApi.cs
@@ -2,8 +2,9 @@ using Refit;
 
 namespace Defra.TradeImportsMessageReplay.MessageReplay.Services;
 
+[Headers("Content-Type: application/json")]
 public interface IImportProcessorApi
 {
     [Post("/replay/import-pre-notifications")]
-    Task SendImportPreNotification([Body] string json);
+    Task SendImportPreNotification([Body] string data);
 }
