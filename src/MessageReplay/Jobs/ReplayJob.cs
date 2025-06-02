572c485f258ed4c7bb547ca40b54390c967b06a2
Thomas Anderson <t11omas1983@live.co.uk>
2025-06-02 07:12:35 +0100

    changed hangfire logging to be async, and only log warnings, plus changed the way messages are processed

--- src/MessageReplay/Jobs/ReplayJob.cs
+++ src/MessageReplay/Jobs/ReplayJob.cs
@@ -20,32 +20,43 @@ public class ReplayJob(
     {
         var files = blobService.GetResourcesAsync(prefix, cancellationToken);
 
-        await foreach (var file in files)
-        {
-            var state = new AwaitingState(
-                context.BackgroundJob.Id,
-                new EnqueuedState(),
-                JobContinuationOptions.OnlyOnSucceededState
-            );
-            jobManager.Create(
-                Job.FromExpression(
-                    () => ProcessBlob(file, Guid.NewGuid().ToString("N"), null!, CancellationToken.None),
-                    context.BackgroundJob.Job.Queue
-                ),
-                state
-            );
-        }
+        await Parallel.ForEachAsync(files,
+            new ParallelOptions() { CancellationToken = cancellationToken, MaxDegreeOfParallelism = 8 },
+            async (file, token) =>
+            {
+               await Task.Run(() => ProcessBlob(file, Guid.NewGuid().ToString("N"), context, token), token);
+            });
     }
 
     [JobDisplayName("Replaying blob - {0}")]
     public async Task ProcessBlob(string file, string traceId, PerformContext context, CancellationToken token)
     {
-        traceContextAccessor.Context = new TraceContext() { TraceId = traceId };
-        logger.LogInformation("TraceId = {TraceId}", traceId);
-        var blobItem = await blobService.GetResource(file, token);
-        foreach (var blobProcessor in blobProcessors.Where(x => x.CanProcess(context.BackgroundJob.Job.Queue)))
+        try
         {
-            await blobProcessor.Process(blobItem);
+            traceContextAccessor.Context = new TraceContext() { TraceId = traceId };
+            logger.LogInformation("TraceId = {TraceId}", traceId);
+            var blobItem = await blobService.GetResource(file, token);
+            foreach (var blobProcessor in blobProcessors.Where(x => x.CanProcess(context.BackgroundJob.Job.Queue)))
+            {
+                await blobProcessor.Process(blobItem);
+            }
+        }
+        catch (Exception)
+        {
+            // swallow exception but add a specific continuation job, so it can be tracked and retried
+            var state = new AwaitingState(
+                context.BackgroundJob.Id,
+                new EnqueuedState(),
+                JobContinuationOptions.OnlyOnSucceededState
+            );
+
+            jobManager.Create(
+                Job.FromExpression(
+                    () => ProcessBlob(file, traceId, null!, CancellationToken.None),
+                    context.BackgroundJob.Job.Queue
+                ),
+                state
+            );
         }
     }
 }
